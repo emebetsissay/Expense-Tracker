2291d3bf779684ff994082f7c50536788500cf73
emebetsissay <61718737+emebetsissay@users.noreply.github.com>
2020-08-27 11:05:06 -0500

    HelloWorld

---
+++ src/main/java/org/luanchcode/ExpenseTracker/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.luanchcode.ExpenseTracker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+    @RestController
+    public class HelloWorld {
+        @RequestMapping("/")
+        public String index() {
+            return "Hello World";
+        }
+    }
+
