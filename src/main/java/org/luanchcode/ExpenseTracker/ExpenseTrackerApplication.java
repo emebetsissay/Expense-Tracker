ecdc1d3033a6902347f91ca850128615bcb4f360
emebetsissay <61718737+emebetsissay@users.noreply.github.com>
2020-08-29 16:43:05 -0500

    HelloWorld

--- src/main/java/org/luanchcode/ExpenseTracker/ExpenseTrackerApplication.java
+++ src/main/java/org/luanchcode/ExpenseTracker/ExpenseTrackerApplication.java
@@ -2,8 +2,10 @@ package org.luanchcode.ExpenseTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
